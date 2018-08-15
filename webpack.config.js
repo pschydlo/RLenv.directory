d42ca9aba86376b29a2adff0a9111ab41a62da3e
ubuntu <paul.schydlo@tecnico.ulisboa.pt>
2018-08-15 15:28:52 +0100

    Updates package versions and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
+const path = require('path');
+
 module.exports = {
   // webpack folder's entry js - excluded from jekll's build process.
   entry: "./webpack/entry.js",
+  mode: 'development',
   output: {
     // we're going to put the generated file in the assets folder so jekyll will grab it.
-      path: 'assets/javascripts/',
+      path: path.join(__dirname, 'assets/javascripts/'),
       filename: "bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015']
+          presets: ["react", "es2015"]
         }
       }
     ]
