76e1df6b6c4984ae2d4c41a0935a8328cf10a932
ubuntu <paul.schydlo@tecnico.ulisboa.pt>
2018-08-25 12:29:57 +0100

    Folder structure clean up

--- webpack/entry.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import {render} from 'react-dom';
-import Hello from './components/Hello';
-import App from './components/App'
-
-//import Button from 'react-bootstrap/lib/Button';
-
-
-render(<App />, document.getElementById('root'));
